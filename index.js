63b6daca26cac7525b20b9cb512c545b56390acb
root <root@LAPTOP-C2G0KSOL.localdomain>
2020-09-22 23:11:03 +0000

    new commit

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ function validate(){
      var address = document.form.address;
 
      if (f_name.value != "" && l_name.value != "" && gender.value != "" && phone.value!= "" && email.value != "" && address.value != "") {
-
-        //validate names
-        //validate email
-        //validate phone number
-        
         var data = "You've entered the following details: \n " + "\n" +
         "Full Name: " +  nametext + "\n" +
         "Gender: " + gender.options[gender.selectedIndex].text + "\n" +
@@ -39,51 +34,44 @@ function validate(){
      
      else{
 
-        window.alert("Please fill all required fields"); 
-        f_name.focus(); 
-        l_name.focus();
-        gender.focus(); 
-        phone.focus(); 
-        email.focus(); 
-        address.focus();
-     }
+        if (f_name.value== "")                                  
+        { 
+            window.alert("Please enter your first name."); 
+            f_name.focus(); 
+            return false; 
+        } 
+        if (l_name.value== "")                                  
+        { 
+            window.alert("Please enter your last name."); 
+            l_name.focus(); 
+            return false; 
+        } 
+        if (gender.value== "")                                  
+        { 
+            window.alert("Please select your gender."); 
+            gender.focus(); 
+            return false; 
+        } 
+        if (phone.value== "")                                  
+        { 
+            window.alert("Please enter your phone number."); 
+            phone.focus(); 
+            return false; 
+        } 
+        if (email.value== "")                                  
+        { 
+            window.alert("Please enter your email."); 
+            email.focus(); 
+            return false; 
+        } 
+        if (address.value== "")                                  
+        { 
+            window.alert("Please enter your address."); 
+            email.focus(); 
+            return false; 
+        }  
 
-/*      if (f_name.value== "")                                  
-     { 
-         window.alert("Please enter your first name."); 
-         f_name.focus(); 
-         return false; 
-     } 
-     if (l_name.value== "")                                  
-     { 
-         window.alert("Please enter your last name."); 
-         l_name.focus(); 
-         return false; 
-     } 
-      if (gender.value== "")                                  
-     { 
-         window.alert("Please select your gender."); 
-         gender.focus(); 
-         return false; 
-     } 
-     if (phone.value== "")                                  
-     { 
-         window.alert("Please enter your phone number."); 
-         phone.focus(); 
-         return false; 
-     } 
-     if (email.value== "")                                  
-     { 
-         window.alert("Please enter your email."); 
-         email.focus(); 
-         return false; 
-     } 
-     if (address.value== "")                                  
-     { 
-         window.alert("Please enter your address."); 
-         email.focus(); 
-         return false; 
-     }  */
+     }
    
   }
     
